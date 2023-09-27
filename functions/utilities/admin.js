c13978e3386d6d1cc226c5f0e61b777ec3b88539
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2023-09-27 14:18:55 -0500

    fat erase of non used files

--- functions/utilities/admin.js
+++
@@ -1,7 +0,0 @@
-// let admin = require("firebase-admin");
-// admin.initializeApp()
-// const db = admin.firestore()
-// // firebase db
-// const realDB = admin.database();
-// module.exports = { admin, db, realDB }; 
- 
