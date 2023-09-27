312bbfca56733eb5cd106c8e4aedd660ddf65b21
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2023-09-27 14:12:45 -0500

    new routes for users, sellers and products and some packages updates

--- functions/index.js
+++ functions/index.js
@@ -2,28 +2,83 @@
 // functions
 const {onRequest} = require("firebase-functions/v2/https");
 // db
-const { db } = require('./utilities/config')
+const { 
+  auth,
+  db,  
+  storage,
+} = require('./firebase/admin')
 
 // EXPRESS
 const app = require('express')()
 
 // CORS
-const cors = require('cors')
-app.use(cors({ origin: true }))
+// const cors = require('cors')
+// app.use(cors({ origin: true }))
 
 // MIDDLEWARES
 // middleware for auth
-// const FBAuth = require('./utilities/fbAuth')
+const firebaseAuth = require('./firebase/firebaseAuth')
 // middleware for coords of statics
-const CoordsOfStatics = require('./utilities/coordsOfStatics')
+const CoordsOfSellers = require('./utilities/coordsOfSellers')
 
-// SELLERS
+////////////////////////////////////////// HANDLERS //////////////////////////////////////////////////
+// users
+const { 
+  signup,   
+  login,  
+} = require('./handlers/users/users');
+
+// products
 const {
   postProductsWithCsvFileToStaticDevices
 } = require('./handlers/products/products');
 
-// SELLERS
-app.post('/', CoordsOfStatics, postProductsWithCsvFileToStaticDevices);
+// sellers
+const {
+  postSeller,
+  postSellerLocal360Image
+} = require('./handlers/sellers/sellers');
+
+//////////////////////////////////////////// +++++++ API REST ROUTES +++++++ ///////////////////////////////////////////
+///////////////////////////////////////////////// USERS /////////////////////////////////////////////////////////////
+// test
+app.get('/',firebaseAuth, (req, res) =>{
+  console.log("hi")
+}); 
+
+
+/* USERS */ 
+
+// signup user
+app.post('/signup', signup);
+// login user   
+app.post('/login', login);
+// post user extra details
+
+
+/* PRODUCTS */
+
+// post or create products of sellers with a .csv file
+app.post('/postCsv', 
+  firebaseAuth, 
+  CoordsOfSellers, 
+  postProductsWithCsvFileToStaticDevices
+);
+
+/* SELLERS */
+
+// post or create a new one seller
+app.post('/postSeller',
+  firebaseAuth,
+  CoordsOfSellers,
+  postSeller
+)
+
+// post or create a 360 image in seller doc
+app.post('/postPic360UrlOnSellerDoc',
+  firebaseAuth,
+  postPic360UrlOnSellerDoc
+)
 
 // export functions
 exports.api = onRequest(app);
