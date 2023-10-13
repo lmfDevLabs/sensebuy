5a7f70d649c5aec746912c28a495fa97ca8ef53c
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2023-10-13 13:26:57 -0500

    some changes to authorize the users flow with cgpt refactorization

--- functions/firebase/firebaseAuth.js
+++ functions/firebase/firebaseAuth.js
@@ -1,38 +1,38 @@
-const { admin, db } = require('./admin')
 
-// middleware to auth private routes
-module.exports = (req,res,next) => {
-    let idToken
-    // check if there is a token in the req, or stop the logic
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
-        idToken = req.headers.authorization.split('Bearer ')[1]
+const { admin, db } = require('./admin');
+
+module.exports = async (req, res, next) => {
+    let idToken;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
     } else {
-        console.error('No token found')
-        return res.status(403).json({ Token: 'Body must not be empty' })
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized' });
     }
 
-    // check if the token in the request is provide for us
-    admin
-        .auth()
-        .verifyIdToken(idToken)
-        .then(decodedToken => {
-            req.user = decodedToken
-            console.log(decodedToken) 
-            return db
-                .collection('users')
-                .where('userId', '==', req.user.uid)
-                .limit(1)
-                .get() 
-        })
-        .then((data) => {
-            req.user.username = data.docs[0].data().username
-            req.user.imgUrl = data.docs[0].data().imgUrl
-            req.user.type = data.docs[0].data().type
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken); // Ensure the second parameter is context-appropriate
+        req.user = decodedToken;
+        // console.log({decodedToken})
+        
+        const userDoc = await db.collection('users').doc(decodedToken.uid).get();
+        
 
-            return next()
-        })
-        .catch((err) => {
-            console.error('Error while verifying token ', err)
-            return res.status(403).json(err)
-        })
-}
+        if (!userDoc.exists) {
+            throw new Error('User not found in the database');
+        }
+        
+        const userData = userDoc.data();
+        req.user.uid = decodedToken.uid
+        req.user.username = userData.username;
+        req.user.type = userData.type;
+        // Additional user data...
+        // console.log({userData})
+    
+        return next();
+    } catch (err) {
+        console.error('Error while verifying token ', err);
+        return res.status(403).json({ error: err.message });
+    }
+};
