01932b308cdd3d69d7da62bc33ffe973f73233e9
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2023-10-13 13:27:45 -0500

    some changes to deal with simulators url ports

--- functions/firebase/admin.js
+++ functions/firebase/admin.js
@@ -1,19 +1,44 @@
 const admin = require('firebase-admin');
-const firebaseConfig = require("./firebaseConfig")
-let serviceAccount = require("../sensebuy-e8add-firebase-adminsdk-e0lb2-1ddd888864.json");
+const firebaseConfig = require("./firebaseConfig");
+let serviceAccount;
+require('dotenv').config();
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    ...firebaseConfig
-});
+try {
+    serviceAccount = require("../sensebuy-e8add-firebase-adminsdk-e0lb2-1ddd888864.json");
+} catch (error) {
+    console.error("Failed to load serviceAccount. Ensure the file exists and is configured correctly.");
+    process.exit(1);
+}
+
+try {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        ...firebaseConfig
+    });
+} catch (error) {
+    console.error("Failed to initialize Firebase Admin SDK:", error.message);
+    process.exit(1);
+}
 
 const auth = admin.auth();
 const db = admin.firestore();
-const storage = admin.storage(); 
+const storage = admin.storage();
+
+if (process.env.NODE_ENV === 'development') {
+    try {
+        db.settings({
+            host: "localhost:8080",
+            ssl: false
+        });
+        console.log("Firestore Emulator is set to use localhost:8080");
+    } catch (error) {
+        console.error("Failed to set Firestore Emulator settings:", error.message);
+    }
+}
 
 module.exports = { 
     admin,
     auth,
     db,
     storage 
-}; 
+};
