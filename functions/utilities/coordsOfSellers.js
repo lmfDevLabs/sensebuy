a2ac155e191061cb09d4b5e1db6105bfa52bc3bd
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2023-10-13 13:18:26 -0500

    some untest chages on the coords middleware for the sellers

--- functions/utilities/coordsOfSellers.js
+++ functions/utilities/coordsOfSellers.js
@@ -1,25 +1,28 @@
 // firebase
-const { db } = require('../../firebase')
+const { db } = require('../../firebase');
 
-// extract companyData
-module.exports = async (req,res,next) => {
-    // extract userHandle
-    const thingId = req.params.coords
-    const sellerId = thingId.split("-").slice(2).toString()
-    // db connection
-    db
-        .doc(`/sellers/:sellerId`)
-        .get()
-        .then((doc) => {
-            const coordsOfStatic = {
-                coords:doc.data().coords
-            }
-            // data to pass to the next middleware
-            res.locals.coordsData = coordsOfStatic
-            return next()
-        })
-        .catch((err) => {
-            console.error('Error while pass the coords of static ', err)
-            return res.status(403).json(err)
-        })
-}
+module.exports = async (req, res, next) => {
+    try { 
+        const userId = req.params.userId;
+        const sellerId = thingId.split("-").slice(2).toString();
+
+        // Fetch data from Firestore
+        const doc = await db.doc(`/sellers/${sellerId}`).get();
+        
+        // Check if document exists
+        if (!doc.exists) {
+            console.error('No document found with id: ', sellerId);
+            return res.status(404).json({ error: 'Seller not found' });
+        }
+
+        // Extract and pass data to the next middleware
+        const coordsOfStatic = {
+            coords: doc.data().coords
+        };
+        res.locals.coordsData = coordsOfStatic;
+        return next();
+    } catch (err) {
+        console.error('Error while fetching seller data: ', err);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+};
