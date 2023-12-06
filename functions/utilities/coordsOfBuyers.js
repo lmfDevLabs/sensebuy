05acb459cc20135f90c284f9a43ca7b88a5f94c9
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2023-12-06 11:57:50 -0500

    just the header of the middleware to obtain the position on the buyers

---
+++ functions/utilities/coordsOfBuyers.js
@@ -0,0 +1,12 @@
+// firebase
+const { db } = require('../firebase/admin');
+// busboy
+const Busboy = require('busboy');
+
+module.exports = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
