b4d5e4c7ebab01a33ab8f6ff33b1e275b7a334df
Carlos Alberto Talero Jacome <proyectoslmf@gmail.com>
2025-08-11 18:42:14 -0500

    Bolier plate files to get where we are right now, in this idea of research over the patterns to create a project with firebase, llms, langsmith and more

--- functions/index.js
+++ functions/index.js
@@ -1,150 +1,114 @@
-// nodejs
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-// FIREBASE
-// functions
-const { onRequest } = require("firebase-functions/v2/https");
-const { setGlobalOptions } = require("firebase-functions/v2");
-const { 
-  admin,
-  auth,
-  db,  
-  storage,
-  functions
-} = require('./firebase/admin')
+// Firebase Functions
+import { onRequest, onCallGenkit } from "firebase-functions/v2/https";
+import { setGlobalOptions } from "firebase-functions/v2";
+import { defineSecret } from "firebase-functions/params";
+import admin from 'firebase-admin';
+// genkit & vertex
+import { startFlowServer } from '@genkit-ai/express';
+
+// Fetch (Node 18+)
+const fetch = globalThis.fetch;
 
 // EXPRESS
-// const express = require('express');
-// const app = express()
-const app = require('express')()
+import express from 'express';
+const app = express();
 
 // CORS
-const cors = require('cors')
-app.use(cors({ origin: true }))
-
-// pdf parse
-const pdfParse = require('pdf-parse');
-
-// mutex
-const { Mutex } = require('async-mutex');
+import cors from 'cors';
+app.use(cors({ origin: true }));
 
 // MIDDLEWARES
-// app.use(express.json());
-// middleware for auth
-const firebaseAuth = require('./firebase/firebaseAuth')
-// middleware for coords of statics
-const coordsOfSellers = require('./utilities/middlewares/coordsOfSellers')
+import firebaseAuth from './firebase/firebaseAuth.js';
+import coordsOfSellers from './utilities/middlewares/coordsOfSellers.js';
 
-// ////////////////////////////////////////// HANDLERS //////////////////////////////////////////////////
-// auth
-// const { 
-//   signup,   
-//   login,  
-// } = require('./handlers/users/users');
-
-// showrooms
-// const {
-//   getShowrooms,
-//   getShowroom,
-//   showrooms,
-//   updateShowroom,
-//   deleteShowroom,
-// } = require('./handlers/showRooms/showRooms');
-
-// // users
-// const {
-//   getUsers,
-//   getUser,
-//   users,
-//   updateUser,
-//   deleteUser
-// } = require('./handlers/users/users');
+// HANDLERS
 
 // sellers
-const {
-  // getSellers,
-  // getSeller,
+import {
   sellers,
-  // updateSeller,
-  // deleteSeller,
   coords,
-  // localImage360
-} = require('./handlers/sellers/sellers');
+} from './handlers/sellers/sellers.js';
 
 // products
-const {
-  // getProducts,
-  // getProduct,
-  // products,
-  // updateProduct,
-  // deleteProduct,
-  // images,
+import {
   xlsx,
   xlsx2,
   xlsx3,
-  docs 
-} = require('./handlers/products/products');
+  xlsx4,
+  xlsx5,
+  docsPdf,
+  docsHtml,
+} from './handlers/products/products.js';
 
-// buyers
-const {
-//   getBuyers,
-//   getBuyer,
+// buyers 
+import {
   buyers,
   tokenBuyers,
-//   updateBuyer,
-//   deleteBuyer
-} = require('./handlers/buyers/buyers');
+} from './handlers/buyers/buyers.js';
 
 // queries
-const {
-//   getQueries,
-//   getQuery,
+import {
   queriesOpenAI,
   queriesOpenAIAndAlgolia,
-//   getQueriesByUser,
-//   getQueryByUser,
-//   updateQuery,
-//   deleteQuery
-} = require('./handlers/queries/queries');
+} from './handlers/queries/queries.js';
 
 // chats
-const {
-  chats,
-  chatWithOpenAIAndAlgolia
-} = require('./handlers/chats/chats');
+import {
+  chatsOnlyLLM,
+  chatWithOpenAIAndAlgolia,
+  chatsNlpApiForDocs,
+  chatsNlpApiForComposeDescription,
+} from './handlers/chats/chats.js';
 
 // whatsapp
-const {
+import {
   getWhats,
-  postWhats
-} = require('./handlers/whatsapp/whatsapp');
+  postWhats,
+} from './handlers/whatsapp/whatsapp.js';
 
-
-// utilities
-const {
-  getEmbeddingsFromOpenAI
-} = require('./utilities/embeddings');
-
-const {
+// UTILITIES
+import {
   downloadDocFromExternalUrl
-} = require('./utilities/externalDocs');
+} from './utilities/externalDocs.js';
 
-const {
-  publishChatMessageForNlp
-} = require('./utilities/pub-sub');
-// //////////////////////////////////////////// +++++++ API REST ROUTES +++++++ ///////////////////////////////////////////
+// flows
+import productSuggestionFlow from './genkit/flows/productSuggestionFlow.js'
+import indexProductItemsFlow from './genkit/flows/indexProductItemsFlow.js'
+import ragProductQuestionFlow from './genkit/flows/ragProductQuestionFlow.js'
+
+// TRIGGERS
+// ops
+// import updateShowRoomOnProductCreate from './triggers/updateShowRoomOnProductCreate.js';
+// import docsProcessingOnProductCreate from './triggers/docsProcessingOnProductCreate.js';
+// import detectTelemetryEventsForAllDevices from './triggers/detectTelemetryEventsForAllDevices.js';
+// flows
+import saveActiveParagraphAsChunk from './triggers/saveActiveParagraphAsChunk.js';
+import queueHtmlDocument from './triggers/queueHtmlDocument.js';
+import processHtmlDocument from './triggers/processHtmlDocument.js';
+// import extractChunksFromProductUrlsOnCreate from './triggers/extractChunksFromProductUrlsOnCreate.js';
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// API REST ROUTES ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 
 // API Version
 const apiVersion = "v1"
+
+// dotenv
+// require('dotenv').config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Set the maximum instances to 10 for all functions
 setGlobalOptions({ maxInstances: 10 });
 
 // test
 app.get(`/${apiVersion}/test`, async (req, res) => {
-  const messageTest = await publishChatMessageForNlp('chats', { sessionId: '12345', userQuery: 'Hola, busco una camioneta 2024' });
-  // res.send('Hello from API REST' + " " + req.user.uid)
+  console.log("Test route")
+  // const messageTest = await publishChatMessageForNlp('chats', { sessionId: '12345', userQuery: 'Hola, busco una camioneta 2024' });
+  let time = admin.firestore.FieldValue.serverTimestamp()
+  res.send(`hi time: ${time}`)
 })
 
 // // git data from sensebuy
@@ -224,20 +188,26 @@ app.post(`/${apiVersion}/sellers/coords`, firebaseAuth, coords);
 // // 5-DELETE /products/:id: Elimina un producto específico.
 // app.delete(`/${apiVersion}/products/:productId`, firebaseAuth, deleteProduct);
 // // 6-POST /products/:productId/images: Para agregar imagenes a un producto.
-// app.post(`/${apiVersion}/products/:productId/images`, firebaseAuth, images);
+// app.post(`/${apiVersion}/products/:productId/images`, firebaseAuth, images); 
 // 7-POST /products/xls: Para agregar productos a través de un archivo xlsx.
 app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/products/xlsx`, firebaseAuth, coordsOfSellers, xlsx);
 // 8-POST /products/xls2: Para agregar productos a través de un archivo xlsx and embbeding stuff.
 app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/products/xlsx2`, firebaseAuth, coordsOfSellers, xlsx2);
 // 9-POST /products/xls3: Para agregar productos a través de un archivo xlsx and embbeding complete cycle.
 app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/products/xlsx3`, firebaseAuth, coordsOfSellers, xlsx3);
-// 10-POST /products/:productId/docs: Para agregar documentos a un producto.
-app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/product/:productId/docs`, docs);
-// 11-Test /testUrlPdf Para probar la creacion de los embeddings de un pdf
+// 10-POST /products/xls4: Para agregar productos a través de un archivo xlsx and embbeding with genkit cycle.
+app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/products/xlsx4`, firebaseAuth, coordsOfSellers, xlsx4);
+// 11-POST /products/xls5: Para agregar productos a través de un archivo xlsx and embbeding with genkit cycle ans langsmith tracers.
+app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/products/xlsx5`, firebaseAuth, coordsOfSellers, xlsx5);
+// 12-POST /products/:productId/docs: Para agregar documentos a un producto.
+app.post(`/${apiVersion}/showroom/:showRoomId/seller/:sellerId/product/:productId/docsPdf`, docsPdf);
+// 13-Test /testUrlPdf Para probar la creacion de los embeddings de un pdf
 app.get(`/${apiVersion}/testUrlPdf`, async (req,res)=>{
   let result = await downloadDocFromExternalUrl(req.body.url)
   console.log({result})
-})
+}) 
+// 14-POST /products/:productId/docs: Para agregar documentos html a un producto.
+app.post(`/${apiVersion}/docsHtml`, firebaseAuth, docsHtml);
 
 /* BUYERS */
 // // super admin
@@ -286,216 +256,72 @@ app.post(`/${apiVersion}/queries2`, firebaseAuth, queriesOpenAIAndAlgolia);
 // app.get(`/${apiVersion}/chats/:chatId`, firebaseAuth, getChat);
 
 // // user admin
-// // 3-POST /chats: Crea un nuevo chat.
-app.post(`/${apiVersion}/chats`, firebaseAuth, chats);
+// // 3-POST /chats: Crea un nuevo chat para interactuar con llm.
+app.post(`/${apiVersion}/chats`, firebaseAuth, chatsOnlyLLM);
+// // 3a-POST /chats-nlp-api: Crea un nuevo chat para interactuar con api nlp.
+app.post(`/${apiVersion}/chats-nlp-api-docs`, firebaseAuth, chatsNlpApiForDocs);
+// // 3b-POST /chats-nlp-api: Crea un nuevo chat para interactuar con api nlp.
+app.post(`/${apiVersion}/chats-nlp-api-compose`, firebaseAuth, chatsNlpApiForComposeDescription);
+
 // // 4-PUT /chats/:id: Actualiza un chat específico.
 // app.put(`/${apiVersion}/chats/:chatId`, firebaseAuth, updateChat);
 // // 5-DELETE /chats/:id: Elimina un chat específico.
 // app.delete(`/${apiVersion}/chats/:chatId`, firebaseAuth, deleteChat);
 // 6-POST /${apiVersion}/chats/:chatId/session/:sessionId accede a un chat ya creado con una busqueda en algolia
-// app.post('/${apiVersion}/chats/session/:sessionId',firebaseAuth, chatWithOpenAIAndAlgolia)
- 
+app.post('/${apiVersion}/chats/session/:sessionId',firebaseAuth, chatWithOpenAIAndAlgolia)
+
 // /* WHATSAPP */
 app.get(`/${apiVersion}/webhook`,getWhats)
 app.post(`/${apiVersion}/webhook`,postWhats)
 
-// /* PRODUCTS SUGESTIONS */
-// // super admin
-// // 1-GET /products/sugestions: Obtiene todos los productos sugeridos.
-// app.get(`/${apiVersion}/products/sugestions`, firebaseAuth, getSugestions);
-// // 2-GET /products/sugestions/:id: Obtiene un producto sugerido específico.
-// app.get(`/${apiVersion}/products/sugestions/:sugestionId`, firebaseAuth, getSugestion);
-
-// // user admin
-// // 3-POST /products/sugestions: Crea un nuevo producto sugerido.
-// app.post(`/${apiVersion}/products-sugestions`, firebaseAuth, sugestions);
-// // 4-PUT /products/sugestions/:id: Actualiza un producto sugerido específico.
-// app.put(`/${apiVersion}/products-sugestions/:sugestionId`, firebaseAuth, updateSugestion);
-// // 5-DELETE /products/sugestions/:id: Elimina un producto sugerido específico.
-// app.delete(`/${apiVersion}/products-sugestions/:sugestionId`, firebaseAuth, deleteSugestion);
-
-
-
 // export functions
-exports.api = onRequest(app);
+const api = onRequest(app);
 
-// triggers - sin testear despues de el ultimo cambio en el que se especifico solo entraran strings en el array de tags
-exports.updateShowRoomOnProductCreate = functions.firestore
-    .document('products/{productId}')
-    .onCreate(async (snap, context) => {
-        console.log('updateShowRoomOnProductCreate');
-        // Obtener los datos del producto recién creado
-        const newProduct = snap.data();
+////////////////////////////////////////////////////////////////////////
+////////////////////////// GENKIT ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // Verificar si el campo 'tags' existe y es un array
-        if (newProduct.tags && Array.isArray(newProduct.tags)) {
-            try {
-                // ID del showroom a actualizar
-                const showRoomId = newProduct.showRoomData.showRoomId;
+// Define una referencia al secreto en Secret Manager
+// const myApiKey = defineSecret(process.env.GOOGLE_AI_API_KEY);
+const GOOGLE_AI_API_KEY_SECRET = defineSecret("GEMINI_API_KEY");
 
-                // Referencia al documento del showroom
-                const showRoomRef = admin.firestore().doc(`showRooms/${showRoomId}`);
 
-                // Obtener el documento actual del showroom
-                const showRoomSnap = await showRoomRef.get();
-
-                if (showRoomSnap.exists) {
-                    const showRoomData = showRoomSnap.data();
-                    let currentTags = showRoomData.tags || [];
-
-                    // Convertir todos los tags a strings y filtrar tipos no deseados
-                    const newTags = newProduct.tags
-                        .map(tag => typeof tag === 'string' ? tag : tag.toString())
-                        .filter(tag => typeof tag === 'string' && !currentTags.includes(tag));
-
-                    if (newTags.length > 0) {
-                        currentTags = [...currentTags, ...newTags];
-                        await showRoomRef.update({ tags: currentTags });
-                    }
-                } else {
-                    console.log('Showroom no encontrado:', showRoomId);
-                }
-            } catch (error) {
-                console.error('Error al actualizar el showroom:', error);
-            }
-        }
-    });
-
-// Escucha los eventos de escritura en la colección "embeddings" - no hace nada por ahora
-exports.listAnyNewEmbeddingOnShowRoomCollection = functions.firestore
-    .document(`showRooms/lsmkexTISq5JzCmIMUi5/embeddings/{embeddingId}`)
-    .onCreate(async(snap,context) => {
-        console.log('New embedding added to the showroom');
-        
+// expose flow on https calls
+startFlowServer({
+  flows: [
+    productSuggestionFlow,
+    indexProductItemsFlow, 
+    ragProductQuestionFlow
+  ],
+  port: 8081,
+  cors: {
+      origin: '*',
+  },
 });
 
-// to create docs embeddings after the product creation
-exports.toCreateProductsDocsEmbeddings = functions.firestore
-  .document('products/{productId}')
-  .onCreate(async (snap, context) => {
-    console.log('toCreateProductsDocsEmbeddings triggered');
-    
-    // Data
-    const newProduct = snap.data();
-    const sellerId = newProduct.sellerData.sellerId;
-    const companyName = newProduct.sellerData.companyName;
-    const pdfUrl = newProduct.pdf;
-    const showRoomId = newProduct.showRoomData.showRoomId;
-    const productId = context.params.productId;
-    
-    // Mutex
-    const mutex = new Mutex();
-    
-    // Bucket
-    const bucket = storage.bucket("gs://sensebuy-e8add.appspot.com/");
-    
-    // Data after buffer
-    let pdfText = null;
 
-    // Función para manejar la descarga y procesamiento del PDF
-    const handlePdfDownload = async (pdfUrl) => {
-      try {
-        const pdfBuffer = pdfUrl ? await downloadDocFromExternalUrl(pdfUrl) : null;
-        if (pdfBuffer) {
-          console.log('PDF descargado con éxito.');
-          const pdfData = await pdfParse(pdfBuffer);
-          return pdfData.text;
-        } else {
-          console.log('No se proporcionó una URL de PDF válida.');
-          return null;
-        }
-      } catch (error) {
-        console.error('Error al descargar o procesar el PDF:', error);
-        return null;
-      }
-    };
+/////////////////////////////// flows calls /////////////////////////////
+// const callProductSuggestionFlow = onCallGenkit(
+//   {
+//     secrets: [GOOGLE_AI_API_KEY_SECRET],
+//     timeoutSeconds: 60,
+//   },
+//   productSuggestionFlow
+// );
 
-    let success = false;
-    let message = '';
 
-    try {
-      // Llamar a la función para manejar la descarga del PDF y obtener el texto
-      pdfText = await handlePdfDownload(pdfUrl);
+export {
+  api,
 
-      // Generar embeddings solo si hay texto del PDF
-      if (pdfText) {
-        console.log('Generando embeddings...');
-        const newEmbeddings = await getEmbeddingsFromOpenAI(pdfText);
-        console.log('Embeddings generados:', newEmbeddings);
+  // triggers
+  // triggers ops
+  // updateShowRoomOnProductCreate,
+  // docsProcessingOnProductCreate,
+  // detectTelemetryEventsForAllDevices,
 
-        // Ruta para guardar en CS
-        const jsonFilePath = `${showRoomId}/docs_sellers/${sellerId}/${sellerId}-${companyName}-embeddings.json`;
-        let embeddingsData = [];
-
-        // Mutex
-        await mutex.runExclusive(async () => {
-          try {
-            const [fileExists] = await bucket.file(jsonFilePath).exists();
-            console.log('Archivo existente:', fileExists);
-            
-            if (fileExists) {
-              const tempFilePath = path.join(os.tmpdir(), `${sellerId}.json`);
-              await bucket.file(jsonFilePath).download({ destination: tempFilePath });
-              const fileContent = fs.readFileSync(tempFilePath, 'utf8');
-              embeddingsData = JSON.parse(fileContent);
-              console.log('Datos de embeddings existentes:', embeddingsData);
-            } else {
-              console.log('No se encontró un archivo existente, se creará uno nuevo.');
-            }
-          } catch (error) {
-            console.error('Error al leer el archivo de embeddings:', error);
-          }
-
-          // Actualizar embeddings
-          embeddingsData.push({
-            companyName,
-            sellerId,
-            productId,
-            vector: newEmbeddings.data[0]
-          });
-          const tempFilePath = path.join(os.tmpdir(), `${sellerId}.json`);
-          fs.writeFileSync(tempFilePath, JSON.stringify(embeddingsData, null, 2));
-          console.log('Embeddings actualizados:', embeddingsData);
-
-          // Subir archivo actualizado con embeddings
-          await uploadFileToCloudStorage(tempFilePath, jsonFilePath);
-          console.log('Embeddings actualizados y subidos con éxito.');
-        });
-
-        success = true;
-        message = 'Embeddings generados y subidos con éxito.';
-      } else {
-        console.log('No se generaron embeddings porque no se pudo obtener el texto del PDF.');
-        success = true;
-        message = 'No se generaron embeddings porque no se pudo obtener el texto del PDF.';
-      }
-    } catch (error) {
-      console.error('Error en el procesamiento:', error);
-      success = false;
-      message = 'Error en el procesamiento: ' + error.message;
-    }
-
-    // Enviar respuesta al usuario
-    return {
-      success,
-      message
-    };
-  });
-
-// detect traffic of gps coords from buyers in topic events and db sync
-exports.detectTelemetryEventsForAllDevices = functions.pubsub.topic('telemetry').onPublish(async (message) => {
-  const payload = message.json;
-  const buyerId = payload.buyerId;
-  console.log({buyerId})
-  try {
-    const buyerDocRef = admin.firestore().collection('buyers').doc(buyerId);
-    await buyerDocRef.update({
-      dataMobilDevice: payload.dataMobilDevice,
-      statusOfBracelet: payload.statusOfBracelet
-    });
-    console.log('Message saved to Firestore for buyer:', buyerId);
-  } catch (error) {
-    console.error('Error saving message to Firestore:', error);
-  }
-});
-
+  // triggers flows
+  saveActiveParagraphAsChunk,
+  queueHtmlDocument,
+  processHtmlDocument
+  // extractChunksFromProductUrlsOnCreate
+}
